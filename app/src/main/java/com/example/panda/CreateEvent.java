8b4910d7b6045b792b8a75d089a2152728a9ee66
Natalie Husby <husby_nata@bentley.edu>
2017-04-23 16:46:31 -0400

    Changed the color  of the background and the text for the tab view, and added a notification for when a new event is created.

--- app/src/main/java/com/example/panda/CreateEvent.java
+++ app/src/main/java/com/example/panda/CreateEvent.java
@@ -2,6 +2,9 @@ package com.example.panda;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -29,6 +32,8 @@ public class CreateEvent extends Activity {
     private String userEventState;
     private String userEventDescription;
     private String userEventWebsite;
+    private Intent notifyIntent;
+    private String message;
 
     private Context context;
 
@@ -48,6 +53,7 @@ public class CreateEvent extends Activity {
         });
 
         this.context = this;
+        notifyIntent = new Intent(this, MainActivity.class);
 
     }
 
@@ -91,6 +97,10 @@ public class CreateEvent extends Activity {
             dialog.setTitle("Congrats!");
             dialog.setMessage("Your event was created");
 
+            message = "A new event named " + userEventTitle + " have been created.";
+
+            showNotification(context);
+
             dialog.setButton(DialogInterface.BUTTON_NEUTRAL, "Close",	new DialogInterface.OnClickListener() {
                 public void onClick(DialogInterface dialog, int whichButton) {
 
@@ -104,6 +114,26 @@ public class CreateEvent extends Activity {
 
         }
 
+    }
+    //Sets and composes the notification
+    public void showNotification(Context context){
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, notifyIntent, PendingIntent.FLAG_ONE_SHOT);
+
+        Notification magicNotification = new Notification.Builder(context)
+                .setContentTitle("New event created")    //set Notification text and icon
+                .setContentText(message)
+                .setSmallIcon(R.drawable.panda)
+                .setTicker("New event created!")
+                .setWhen(System.currentTimeMillis())
+                .setContentIntent(pendingIntent)
+                .setVibrate(new long[] {1000, 1000, 1000, 1000})
+                .setLights(Integer.MAX_VALUE,  500,  500)
+                .setAutoCancel(true)                                // auto close notification on click
+                .build();
+
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(0, magicNotification);
     }
 
 }
